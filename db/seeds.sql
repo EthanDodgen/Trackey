4f88bdf66d378ff55a9c11574d7c51b2ceb53980
Fival <thefivalco@gmail.com>
2021-05-07 08:36:06 -0500

    update employee role callback

--- db/seeds.sql
+++ db/seeds.sql
@@ -8,8 +8,8 @@ VALUES
 INSERT INTO role (title, salary, department_id )
 VALUES
  ("Engineer", "90", "1"),
- ("HR", "5", "20"),
- ("Maintenance", "30", "3"),
+ ("Junior", "5", "20"),
+ ("Senior", "30", "3"),
  ("Manager", "80", "4");
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
